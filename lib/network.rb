872cf93f3d3990d3d6cd7e1d266d34a159a50c40
Kate Tester <katemorris@gmail.com>
2020-09-01 09:59:21 -0600

    Adds class data for everything up to actors by show

---
+++ lib/network.rb
@@ -0,0 +1,28 @@
+class Network
+
+  attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def all_characters
+    @shows.map { |show| show.characters }.flatten
+  end
+
+  def main_characters
+    all_characters.select do |character|
+      character.salary > 500000 && character.name == character.name.upcase
+    end
+  end
+
+  def actors_by_show
+
+  end
+
+end
